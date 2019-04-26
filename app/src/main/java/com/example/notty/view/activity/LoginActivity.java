9aceb0149060e96ad65f4876285addc7d25f6702
RafalskyCodit <codit.rafalsky@gmail.com>
2019-04-26 11:24:37 +0300

    initial commit

---
+++ app/src/main/java/com/example/notty/view/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.notty.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.notty.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
